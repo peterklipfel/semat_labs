dfaac1146cee5c7654af9890ffd5a24daa66f8eb
Peter Klipfel <peter@klipfel.me>
2013-09-11 17:22:06 -0600

    rps tournament copmletes

--- Lab2.rb
+++ Lab2.rb
@@ -31,3 +31,43 @@ end
 raise "Failed" unless (count_words("A man, a plan, a canal -- Panama") == {'a' => 3, 'man' => 1, 'canal' => 1, 'panama' => 1, 'plan' => 1})
 raise "Failed" unless (count_words("Doo bee doo bee doo") == {'doo' => 3, 'bee' => 2})
 
+
+class WrongNumberOfPlayersError <  StandardError ; end
+class NoSuchStrategyError <  StandardError ; end
+
+def rps_game_winner(game) # game = [["p1", "r"], ["p2", "p"]]
+  raise WrongNumberOfPlayersError unless game.length == 2
+  moves = [game[0][1].downcase, game[1][1].downcase]
+  moves.each {|m| raise NoSuchStrategyError unless ['r', 'p', 's'].include? m }
+  return game[0] if [['s', 'p'], ['p', 'r'], ['r', 's'], ['r', 'r'], ['p', 'p'], ['s', 's']].include? moves
+  return game[1] if [['p', 's'], ['r', 'p'], ['s', 'r']].include? moves
+end
+
+raise "Failed" unless rps_game_winner([ ["Armando", "P"], ["Dave", "S"] ]) == ["Dave", "S"]
+
+def get_set_winner set
+  first_winner = rps_game_winner set[0]
+  second_winner = rps_game_winner set[1]
+  set_winner = rps_game_winner [[first_winner, moves[first_winner]], [second_winner, moves[second_winner]] ]
+end
+
+def rps_tournament_winner tournie
+  # inspiration from http://stackoverflow.com/questions/14388700/ruby-rock-paper-scissors-tournament
+  return rps_game_winner(tournie) if tournie[0][0].kind_of? String
+  return rps_game_winner([rps_tournament_winner(tournie[0]),rps_tournament_winner(tournie[1])])
+end
+
+raise "Failed" unless rps_tournament_winner(
+  [
+      [
+          [ ["Armando", "P"], ["Dave", "S"] ],
+          [ ["Richard", "R"],  ["Michael", "S"] ],
+      ],
+      [
+          [ ["Allen", "S"], ["Omer", "P"] ],
+          [ ["David E.", "R"], ["Richard X.", "P"] ]
+      ]
+  ]
+) == ["Richard", "R"]
+
+
