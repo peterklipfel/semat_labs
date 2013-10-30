de277915e20d3835d24c2df2ed62ded8c3245d2f
Peter Klipfel <peter@klipfel.me>
2013-10-30 16:12:27 -0600

    initialized rails app for lab 4 part 2

---
+++ lab4-2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab42::Application.config.secret_token = '4e9662e9adbb6ac4099f01317c5ee2db18afa951fb3151c9b2f2b4ae7a4d23082caf3d6983b03e70d3c0c3dca625e1ee67c5a83b9603d19eca3a3ce5136cf1ad'
