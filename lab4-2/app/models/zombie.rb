2f7fb12554f7a71262142c1924b16d084590f499
Peter Klipfel <peter@klipfel.me>
2013-10-30 17:10:31 -0600

    finished second section of second part of lab 4 finished

--- lab4-2/app/models/zombie.rb
+++ lab4-2/app/models/zombie.rb
@@ -1,12 +1,15 @@
 class Zombie < ActiveRecord::Base
-  attr_accessor :name, :tweets
+  attr_accessor :name, :tweets, :brains
   has_many :tweets
   has_many :weapons
   validates :name, :presence => true
 
   def initialize(attributes=nil)
     super(attributes)
-    weapons << Weapon.new
-    weapons << Weapon.new
+    @name = 'Ash'
+    
+    tweet1 = Tweet.new(status: 'Unh')
+    tweet2 = Tweet.new(status: 'Arg')
+    self.tweets << tweet1 << tweet2
   end
 end
